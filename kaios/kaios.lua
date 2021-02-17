01408280e83ef48d84c5d7b09995ea3a9688d0a5
Björn Ritzl <bjorn.ritzl@gmail.com>
2021-02-18 00:07:40 +0100

    Check KAIOS user agent

--- kaios/kaios.lua
+++ kaios/kaios.lua
@@ -1,3 +1,10 @@
+local info = sys.get_sys_info()
+
+if not info.user_agent:find("KAIOS") then
+	kaios = nil
+	return
+end
+
 function kaios.exit()
 	sys.exit(0)
 	if html5 then html5.run("window.close()") end
@@ -5,7 +12,7 @@ end
 
 
 function kaios.play_sound(file)
-	if html5 then html5.run("kaios_playAudioFromURL('" .. sys.get_application_path() .. "/" .. url .. "');") end
+	if html5 then html5.run("kaios_playAudioFromURL('" .. sys.get_application_path() .. "/" .. file .. "');") end
 end
 
 return kaios
